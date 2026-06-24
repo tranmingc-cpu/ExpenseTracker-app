f681e2caab98fb6b14000f6103b77a601fda880b
tranmingc-Rinkito <tranmingc@gmail.com>
2026-06-24 07:45:46 +0700

    lay ma qr va thanh toan

---
+++ ExpenseTracker_app/app/src/main/java/com/expensetracker_manager/network/api/PaymentApiService.java
@@ -0,0 +1,13 @@
+package com.expensetracker_manager.network.api;
+
+import com.expensetracker_manager.model.request.PaymentLinkRequest;
+import com.expensetracker_manager.model.response.PaymentLinkResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface PaymentApiService {
+    @POST("api/payment/generate-links")
+    Call<PaymentLinkResponse> generateLinks(@Body PaymentLinkRequest request);
+}
